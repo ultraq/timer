471a605a1e1a08813356b342b46ccd7c09f75bfe
Emanuel Rabina <emanuelrabina@gmail.com>
2018-04-07 12:37:43 +1200

    Add copyright block now that this project is out in the wild

--- Timer.js
+++ Timer.js
@@ -1,3 +1,18 @@
+/* 
+ * Copyright 2015, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * Interval between timer checks to see if the resolution between ticks has
@@ -7,13 +22,15 @@
  *       rid of this.  And then the Animation class can just make use of this as
  *       the timing mechanism.
  * 
- * @const {number}
+ * @const {Number}
  */
 const DEFAULT_CHECK_INTERVAL = 50;
 
 /**
  * Basic timer class that executes a function which is passed the time elapsed
  * since the timer was started.
+ * 
+ * @author Emanuel Rabina
  */
 export default class Timer {
 
